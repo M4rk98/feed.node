61f37294a60e4ddb5d7ea96a6efd186f68775f4a
Mark <mark.gangel@agcocorp.com>
2019-11-25 19:47:57 +0100

    interceptors for requests, identify request user

---
+++ src/util/RequestUser.js
@@ -0,0 +1,16 @@
+import {AuthenticationError} from "apollo-server-errors";
+import jwt from 'jsonwebtoken';
+
+export const requestUser = async req => {
+    const token = req.headers['x-token'];
+
+    if (token) {
+        try {
+            return await jwt.verify(token, process.env.SECRET);
+        } catch (e) {
+            throw new AuthenticationError(
+                'Your session expired. Sign in again.',
+            );
+        }
+    }
+};
